12a5c8e2c99866fc626c0d7db757015413fe45b4
amirnkhan <aamirnawazkhn@gmail.com>
2021-08-19 16:40:26 +0530

    hello all

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+function Footer() {
+    return (
+        <div>
+            <footer className='footer1'>
+            <h5 className='hed'> ©️ aamir@gmail.com ❤️</h5>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
